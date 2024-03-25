e75c5263c27869bfd1f14d829edea5bafc584723
vinay khimsuriya <khimsuriyavinay29@gmail.com>
2024-03-25 11:10:39 +0530

    Project is being

---
+++ index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM  from "react-dom/client";
+import App from "./src/App";
+
+
+// const heading=React.createElement(<App/>);
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App/>);
---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+      <h1>તુ તો સાવ ગાંડો છે ભાઈ. અને વિનય ખિમસુરિયા તો બોવ જ સમજદાર છે </h1>
+    </div>
+  )
+}
